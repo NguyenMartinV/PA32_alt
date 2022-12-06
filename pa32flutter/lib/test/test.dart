cfcd8e21d8e610cfee52346a1a6bd0e25557bff3
Kailin Chen <kc2637@nau.edu>
2022-12-06 16:35:19 -0600

    updated test.dart

--- pa32flutter/lib/test/test.dart
+++ pa32flutter/lib/test/test.dart
@@ -16,7 +16,6 @@ class TestPage extends StatefulWidget {
 }
 
 class _TestPageState extends State<TestPage> {
-  bool flag = true;
   List myCaresData = [];
   List myGivers = [];
   List myEmergency = [];
@@ -27,6 +26,8 @@ class _TestPageState extends State<TestPage> {
   String token = '';
   int MyswiperIndex = 0;
 
+  bool flag = true;
+  int swiperIndex = 0;
   List myCustomerData = [];
   List myAlertsData = [];
   String pageFrom = '';
@@ -127,6 +128,175 @@ class _TestPageState extends State<TestPage> {
       flag = false;
     }
 
-    return Container();
+    return Center(
+      child: Column(
+        children: [
+          if(myCaresData.isNotEmpty)
+            Container(
+              width: 260,
+              height: 80,
+              margin: EdgeInsets.only(top: 20, bottom: 10),
+              child: Swiper(
+                loop: false,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: EdgeInsets.only(top: 15, bottom: 15, left: 25, right: 10),
+                    child: Container(
+                      width: 100,
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 19, height: 40,
+                            margin: EdgeInsets.only(right: 5),
+                            child: Image(
+                              image: AssetImage("assets/images/pa32.png"),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+
+                          Column(
+                            children: [
+                              Container(
+                                width: 190,
+                                margin: EdgeInsets.only(top: 5, bottom: 5, left: 5),
+                                child: Text(
+                                  "${myCustomerData[swiperIndex].nickname}",
+                                  style: TextStyle(fontSize: 17),
+                                ),
+                              ),
+
+                              Container(
+                                width: 190,
+                                margin: EdgeInsets.only(left: 5),
+                                child: Text(
+                                  "Device No. ${myCustomerData[swiperIndex].nickname}",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Color(0xff999999),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                itemCount: myCustomerData.length,
+                onIndexChanged: (Value) {
+                  setState(() {
+                    swiperIndex = Value;
+                  });
+
+                  _customerDetail(myCustomerData[swiperIndex].id, 15, 15, "0:1,3:10");
+                },
+              ),
+            ),
+
+          if(myCustomerData.isNotEmpty)
+            Container(
+              width: myCustomerData.length * 15,
+              margin: EdgeInsets.only(bottom: 20),
+              child: Row(
+                children: List.generate(myCustomerData.length, (index) => Container(
+                  width: 9, height: 9,
+                  margin: EdgeInsets.only(left: 3, right: 3),
+                  decoration: BoxDecoration(
+                    color: swiperIndex == index
+                        ? Color(0xff028AFE)
+                        : Color(0xffC2C2C2),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                )),
+              ),
+            ),
+
+          myAlertsData.isNotEmpty
+            ? Container(
+            width: 350, height: 500,
+            child: ListView(
+              children: List.generate(myAlertsData.length, (index) => Container(
+                margin: EdgeInsets.only(bottom: 10),
+                padding: EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(bottom: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Container(
+                                width: 18, height: 18,
+                                margin: EdgeInsets.only(right: 10),
+                                child: Image(
+                                  image: AssetImage("assets/images/alarm.png"),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+
+                              Text("${myAlertsData[index].alarmTime.substring(5, 16)}"),
+                              SizedBox(width: 10,),
+                              Text("${myAlertsData[index].alarmTypeName}"),
+                            ],
+                          ),
+
+                          if(myAlertsData[index].lng != null)
+                            GestureDetector(
+                              child: Container(
+                                width: 15, height: 16,
+                                margin: EdgeInsets.only(right: 5),
+                                child: Image(
+                                  image: AssetImage("assets/images/map.png"),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              onTap: () {
+                                Navigator.pushNamed(
+                                  context,
+                                  "addressAlertPage",
+                                  arguments: {
+                                    "currentData": myAlertsData[index],
+                                    "pageWhere": "AlertPage"
+                                  },
+                                );
+                              },
+                            )
+                        ],
+                      ),
+                    ),
+
+                    Container(
+                      width: 265,
+                      child: Text(
+                        "${myAlertsData[index].alarmContent}",
+                        maxLines: 1,
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          overflow: TextOverflow.ellipsis,
+                          color: Color(0xff999999),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )),
+            ),
+          )
+              : Text("No Alert Information"),
+
+        ],
+      ),
+    );
   }
 }
